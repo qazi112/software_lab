1b516a13df72f35ad985be7c47bdb4b7e7f6451d
qazi112 <qarsalan13@gmail.com>
2020-10-09 10:26:28 +0000

    Changed Constructor and Added Function

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 # Hello this is my .py file for oop
 class Person():
-    def __init__(self,name):
+    def __init__(self,name,age):
         self.name = name
-    
+        self.age = age
+    def get_person_details(self):
+        return self.name,self.age
